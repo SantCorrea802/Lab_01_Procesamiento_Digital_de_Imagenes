27f1531fda8055c74a3e6bbef90491ff881c6430
SantCorrea802 <santiago.correa7@udea.edu.co>
2025-04-06 18:17:41 -0500

    Se han seleccionado los valores upper y lower para la mascara con la ayuda de los trackbars de el commit anterior y ahora podemos mostrar la mascara invertida

--- Lab_01.py
+++ Lab_01.py
@@ -26,22 +26,10 @@ cv2.setMouseCallback('Video', mouse_callback)
 
 nuevo_alto = 480
 nuevo_ancho = 680
-upper = np.array([188,91,43])
-lower = np.array([185,88,40])
-
-def nothing(x):
-    pass
+lower = np.array([107,0,0])
+upper = np.array([128,255,239])
 
 
-
-# Crear una ventana con controles deslizantes (trackbars)
-cv2.namedWindow("Trackbars")
-cv2.createTrackbar("H Min", "Trackbars", 0, 179, nothing)
-cv2.createTrackbar("H Max", "Trackbars", 179, 179, nothing)
-cv2.createTrackbar("S Min", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("S Max", "Trackbars", 255, 255, nothing)
-cv2.createTrackbar("V Min", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("V Max", "Trackbars", 255, 255, nothing)
 
 
 while True:
@@ -59,25 +47,14 @@ while True:
         
     frame3 = cv2.resize(frame2, (nuevo_ancho,nuevo_alto))
     hsv = cv2.cvtColor(frame3, cv2.COLOR_BGR2HSV)
+
     
-    # Obtener valores de los trackbars
-    # Leer valores de las trackbars
-    h_min = cv2.getTrackbarPos("H Min", "Trackbars")
-    h_max = cv2.getTrackbarPos("H Max", "Trackbars")
-    s_min = cv2.getTrackbarPos("S Min", "Trackbars")
-    s_max = cv2.getTrackbarPos("S Max", "Trackbars")
-    v_min = cv2.getTrackbarPos("V Min", "Trackbars")
-    v_max = cv2.getTrackbarPos("V Max", "Trackbars")
+    mask = cv2.inRange(hsv, lower,upper)
+    mask = 255-mask
     
-    bajo = np.array([h_min, s_min, v_min])
-    alto = np.array([h_max,s_max,v_max])
+    cv2.imshow('Video', mask)
     
-    mask = cv2.inRange(hsv, bajo,alto)
-    
-    cv2.imshow('video mascara', mask)
-    cv2.imshow('Video', hsv)
-    
-    key = cv2.waitKey(33) & 0xFF
+    key = cv2.waitKey(30) & 0xFF
     if key == 27:  # Presiona la tecla 'Esc' para salir
         break
     
